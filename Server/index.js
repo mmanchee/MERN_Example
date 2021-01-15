a66db55d8327f919d21d8f172c89fffa07827d87
Mike Manchee <mikemanchee@gmail.com>
2021-01-15 10:02:24 -0800

    add try catch to routes for get and post

--- Server/index.js
+++ Server/index.js
@@ -19,6 +19,6 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-  .catch((err) => console.log(err.message));
+  .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
