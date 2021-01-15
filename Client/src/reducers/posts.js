7145b29a7a0b4312679a9a759e6b46787f403c91
Mike Manchee <mikemanchee@gmail.com>
2021-01-15 11:59:22 -0800

    add actions to get

--- Client/src/reducers/posts.js
+++ Client/src/reducers/posts.js
@@ -1,7 +1,7 @@
 export default (posts = [], action) => {
   switch (action.key) {
     case 'FETCH_ALL':
-      return posts;
+      return action.payload;
     case 'CREATE':
       return posts;
     default:
