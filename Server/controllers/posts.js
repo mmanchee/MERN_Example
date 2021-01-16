7a2e736f84c8bd30c733d79563949254e27cd830
Mike Manchee <mikemanchee@gmail.com>
2021-01-15 20:52:22 -0800

    try and debug server side mongodb connection

--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Mongoose } from 'mongoose';
+import mongoose from 'mongoose';
 
 import PostMessage from '../models/postMessage.js';
 
--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://mmanchee:Mmz7xm1!@cluster0.qhwn3.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const CONNECTION_URL = `${process.env.MONGO_DB}`;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
