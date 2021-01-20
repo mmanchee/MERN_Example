76ddcd635ae72cb992534b17b67537a42c11f8b2
Mike Manchee <mikemanchee@gmail.com>
2021-01-19 17:11:46 -0800

    move top nav to NavBar

--- Client/src/App.js
+++ Client/src/App.js
@@ -19,10 +19,7 @@ const App = () => {
 
   return (
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-        <img className={classes.image} src={memories} alt="memories" height="60" />
-      </AppBar>
+      
       <Grow in>
         <Container>
           <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
