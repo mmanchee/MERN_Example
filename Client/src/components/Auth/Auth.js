9542b3752192dcc99ca550aecbc481acf767b6f6
Mike Manchee <mikemanchee@gmail.com>
2021-01-19 21:25:53 -0800

    add Auth and Input as a form component

--- Client/src/components/Auth/Auth.js
+++ Client/src/components/Auth/Auth.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Avatar, Paper, Grid, Typography, Button, Container, TextField } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { GoogleLogin } from 'react-google-login';
+import LockOutlinedIcon from '@material-ui/icons';
+
+import Icon from './icon';
+import { signin, signup } from '../../actions/auth';
+import { Auth } from '../../actions/actionTypes';
+import useStyles from './styles';
+import Input from './Input';
 
 const Auth = () => {
-  const state = null;
+  const classes = useStyles();
+
+  const handleSubmit = () => {
+
+  };
+
+  const handleChange = () => {
+
+  };
 
   return (
-    <div>
-      AUTH
-    </div>
+    <Container component='main' maxWidth='xs'>
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant='h5'>{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {
+              isSignup && (
+                <>
+                  <Input name='firstName' label='First Name' handleChange={handleChanger} autoFocus xs={half}></Input>
+                  <Input name='firstName' label='First Name' handleChange={handleChanger} autoFocus xs={6}></Input>
+                </>
+              )
+            }
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
   )
 };
 
