21fe375d2f6d8961cd2af8e38402db582f1414ce
Mike Manchee <mikemanchee@gmail.com>
2021-01-20 19:31:08 -0800

    add to index in actions

--- Client/src/api/index.js
+++ Client/src/api/index.js
@@ -1,5 +1,14 @@
 import axios from 'axios';
 
+const API = axios.create({ baseURL: 'https://'});
+
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem('profile')) {
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+  }
+  return req;
+});
+
 const url = 'http://localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
@@ -7,3 +16,6 @@ export const createPost = (post) => axios.post(url, post);
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
 export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
+
+export const signIn = (formData) => API.post('/user/signin', formData);
+export const signUp = (formData) => API.post('/user/signup', formData);
