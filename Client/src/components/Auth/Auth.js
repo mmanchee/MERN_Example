970e6db3bd337efd9ad369b02a20abb24046e6b9
Mike Manchee <mikemanchee@gmail.com>
2021-01-20 19:24:32 -0800

    add auth actions

--- Client/src/components/Auth/Auth.js
+++ Client/src/components/Auth/Auth.js
@@ -72,7 +72,7 @@ const SignUp = () => {
             { isSignup ? 'Sign Up' : 'Sign In' }
           </Button>
           <GoogleLogin 
-            clientId={process.env.GOOGLE_CLIENT_ID}
+            clientId={ process.env.REACT_APP_GOOGLE_ID }
             render={(renderProps) => (
               <Button className={classes.googleButton} color='primary' fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant='contained'>Google Sign In</Button>
             )}
